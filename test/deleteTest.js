2eab145cf038a0e76159c84b95c9f194333d024f
iaindurham <iain@cloudconformity.com>
2021-06-28 23:25:57 +1000

    chore: OP-546 uplift repo to add linting and husky
    - fix linting errors

--- test/deleteTest.js
+++ test/deleteTest.js
@@ -1,110 +1,114 @@
-var chai = require("chai");
-	chai.config.includeStack = true;
+const mockAWSSinon = require("mock-aws-sinon");
 
-var chaiAsPromised = require("chai-as-promised");
-	chai.use(chaiAsPromised);
-
-var should = chai.should(),
-	expect = chai.expect;
-
-var mockAWSSinon = require('mock-aws-sinon');
-
-describe("Testing backoff", function() {
-
+describe("Testing backoff", function () {
 	this.timeout(50000);
 
-    describe("base test", function() {
+	describe("base test", function () {
+		const SimpleDynamo = require("../index");
+		const database = new SimpleDynamo(
+			{},
+			{
+				region: "eu-west-1",
+				dynamodb: "2012-08-10"
+			}
+		);
 
-    	SimpleDynamo = require('../index.js');
-		database = new SimpleDynamo({}, {
-		    region: "eu-west-1",
-		    dynamodb: '2012-08-10'
-		});
+		it("Remove should return UnprocessedItems", function (done) {
+			let i = 0;
+			const items = [];
 
-        it("Remove should return UnprocessedItems", function(done) {
-
-        	var i = 0,
-        		items = [];
-
- 			while (i < 200) {
-
- 				items.push({
- 					"something": "cool"
- 				});
-
- 				i++;
- 			}
-
-
-			mockAWSSinon('DynamoDB','batchWriteItem')
-
-				.returns({
-					UnprocessedItems: {"test": items.splice(0, (Math.random() * 2 | 0))}
-				})
-				.onCall(0).returns({
-						UnprocessedItems: {"test": items.splice(0, (Math.random() * 2 | 0))}
-				})
-				.onCall(1).returns({
-						UnprocessedItems: {"test": items.splice(0, (Math.random() * 2 | 0))}
-				})
-				.onCall(2).returns({
-						UnprocessedItems: {}
-				})
-				.onCall(3).returns({
-						UnprocessedItems: {"test": items.splice(0, (Math.random() * 2 | 0))}
-				})
-				.onCall(4).returns({
-						UnprocessedItems: {}
-				})
-				.onCall(5).returns({
-						UnprocessedItems: {"test": items.splice(0, (Math.random() * 2 | 0))}
-				})
-				.onCall(6).returns({
-						UnprocessedItems: {"test": items.splice(0, (Math.random() * 2 | 0))}
-				})
-				.onCall(7).returns({
-						UnprocessedItems: {}
-				})
-				.onCall(8).returns({
-						UnprocessedItems: {}
-				})
-				.onCall(9).returns({
-						UnprocessedItems: {"test": items.splice(0, (Math.random() * 2 | 0))}
-				})
-				.onCall(10).returns({
-						UnprocessedItems: {}
-				})
-				.onCall(11).returns({
-						UnprocessedItems: {}
-				})
-				.onCall(12).returns({
-						UnprocessedItems: {"test": items.splice(0, (Math.random() * 2 | 0))}
-				})
-				.onCall(13).returns({
-						UnprocessedItems: {}
-				})
-				.onCall(14).returns({
-						UnprocessedItems: {}
-				})
-				.onCall(15).returns({
-						UnprocessedItems: {"test": items.splice(0, (Math.random() * 2 | 0))}
-				})
-				.onCall(16).returns({
-						UnprocessedItems: {}
-				})
-				.onCall(17).returns({
-						UnprocessedItems: {}
-				})
-				.onCall(18).returns({
-						UnprocessedItems: {"test": items.splice(0, (Math.random() * 2 | 0))}
+			while (i < 200) {
+				items.push({
+					something: "cool"
 				});
 
+				i++;
+			}
 
-        	var removeOperation = database.removeItems("test", items).then(function() {
-        		done();
-        	});
+			mockAWSSinon("DynamoDB", "batchWriteItem")
+				.returns({
+					UnprocessedItems: { test: items.splice(0, (Math.random() * 2) | 0) }
+				})
+				.onCall(0)
+				.returns({
+					UnprocessedItems: { test: items.splice(0, (Math.random() * 2) | 0) }
+				})
+				.onCall(1)
+				.returns({
+					UnprocessedItems: { test: items.splice(0, (Math.random() * 2) | 0) }
+				})
+				.onCall(2)
+				.returns({
+					UnprocessedItems: {}
+				})
+				.onCall(3)
+				.returns({
+					UnprocessedItems: { test: items.splice(0, (Math.random() * 2) | 0) }
+				})
+				.onCall(4)
+				.returns({
+					UnprocessedItems: {}
+				})
+				.onCall(5)
+				.returns({
+					UnprocessedItems: { test: items.splice(0, (Math.random() * 2) | 0) }
+				})
+				.onCall(6)
+				.returns({
+					UnprocessedItems: { test: items.splice(0, (Math.random() * 2) | 0) }
+				})
+				.onCall(7)
+				.returns({
+					UnprocessedItems: {}
+				})
+				.onCall(8)
+				.returns({
+					UnprocessedItems: {}
+				})
+				.onCall(9)
+				.returns({
+					UnprocessedItems: { test: items.splice(0, (Math.random() * 2) | 0) }
+				})
+				.onCall(10)
+				.returns({
+					UnprocessedItems: {}
+				})
+				.onCall(11)
+				.returns({
+					UnprocessedItems: {}
+				})
+				.onCall(12)
+				.returns({
+					UnprocessedItems: { test: items.splice(0, (Math.random() * 2) | 0) }
+				})
+				.onCall(13)
+				.returns({
+					UnprocessedItems: {}
+				})
+				.onCall(14)
+				.returns({
+					UnprocessedItems: {}
+				})
+				.onCall(15)
+				.returns({
+					UnprocessedItems: { test: items.splice(0, (Math.random() * 2) | 0) }
+				})
+				.onCall(16)
+				.returns({
+					UnprocessedItems: {}
+				})
+				.onCall(17)
+				.returns({
+					UnprocessedItems: {}
+				})
+				.onCall(18)
+				.returns({
+					UnprocessedItems: { test: items.splice(0, (Math.random() * 2) | 0) }
+				});
 
+			database.removeItems("test", items).then(function () {
+				done();
+			});
 		});
-
-    });
+	});
 });
--- test/test.js
+++ test/test.js
@@ -1,94 +1,49 @@
-var Promise = require("bluebird");
-var proxyquire = require("proxyquire");
+const Promise = require("bluebird");
+const proxyquire = require("proxyquire");
 
-var chai = require("chai");
-var should = chai.should(),
-	expect = chai.expect;
+const chai = require("chai");
+const expect = chai.expect;
 
-var utils = require("../utils.js");
+chai.should();
 
-describe("Node simple dynamo", function() {
+const utils = require("../utils");
 
-    describe("Service invocation", function() {
+const __basePath = process.env.PWD;
+global.__basePath = process.env.PWD;
 
-    	SimpleDynamo = require('../index.js');
-		database = new SimpleDynamo({
-		    region: "eu-west-1",
-		    dynamodb: '2012-08-10'
+describe("Node simple dynamo", function () {
+	describe("Service invocation", function () {
+		const SimpleDynamo = require("../index");
+		const database = new SimpleDynamo({
+			region: "eu-west-1",
+			dynamodb: "2012-08-10"
 		});
 
-        it("Inokation should be ok", function() {
+		it("Invocation should be ok", function () {
 			expect(database).to.be.ok;
 		});
-
-    });
-
-    describe("Database Query", function() {
-
-    	var ddbriver;
-
-		before(function() {
-			ddbriver = proxyquire(__basePath + "/index.js", {
-				"aws-sdk": {
-					DynamoDB: function() {
-						return {
-							query: function(params) {
-
-								return {
-									promise: () => Promise.resolve({ Items: [{
-										id: {"S": "string"},
-										number: {"N": "1234"}
-									}]})
-								};
-
-							}
-						};
-					}
-				}
-			});
-		});
-
-		it("should return a correct query object", function() {
-
-			database = new ddbriver({
-			    region: "eu-west-1",
-			    dynamodb: '2012-08-10'
-			});
-
-			return database.query("ContainerUser", [{
-				key: "user",
-				operator: "EQ",
-				value: "test"
-			}], "user-index").then(function(data) {
-
-				data.should.have.length(1);
-				data.should.have.deep.property("[0].id", "string");
-				data.should.have.deep.property("[0].number", 1234);
-
-			});
-
-		});
-
 	});
 
-    describe("Databse list", function() {
+	describe("Database Query", function () {
+		let ddbriver;
 
-    	var ddbriver;
-
-		before(function() {
+		before(function () {
 			ddbriver = proxyquire(__basePath + "/index.js", {
 				"aws-sdk": {
-					DynamoDB: function() {
+					DynamoDB: function () {
 						return {
-							scan: function(params) {
-
+							query: function () {
 								return {
-									promise: () => Promise.resolve({ Items: [{
-										id: {"S": "string"},
-										number: {"N": "1234"}
-									}]})
+									promise: () =>
+										Promise.resolve({
+											Items: [
+												{
+													id: { S: "string" },
+													number: { N: "1234" }
+												}
+											]
+										})
 								};
-
 							}
 						};
 					}
@@ -96,45 +51,52 @@ describe("Node simple dynamo", function() {
 			});
 		});
 
-		it("Should return the correct list", function() {
-
-			database = new ddbriver({
-			    region: "eu-west-1",
-			    dynamodb: '2012-08-10'
+		it("should return a correct query object", function () {
+			const database = new ddbriver({
+				region: "eu-west-1",
+				dynamodb: "2012-08-10"
 			});
 
-			return database.list("is-users", [{
-				key: "email",
-				operator: "EQ",
-				value: "example@super.cool"
-			}]).then(function(data) {
-
-				data.should.have.length(1);
-				data.should.have.deep.property("[0].id", "string");
-				data.should.have.deep.property("[0].number", 1234);
-
-			});
-
+			return database
+				.query(
+					"ContainerUser",
+					[
+						{
+							key: "user",
+							operator: "EQ",
+							value: "test"
+						}
+					],
+					"user-index"
+				)
+				.then(function (data) {
+					data.should.have.length(1);
+					data.should.have.deep.property("[0].id", "string");
+					data.should.have.deep.property("[0].number", 1234);
+				});
 		});
-
 	});
 
-    describe("Databse get", function() {
+	describe("Databse list", function () {
+		let ddbriver;
 
-		before(function() {
+		before(function () {
 			ddbriver = proxyquire(__basePath + "/index.js", {
 				"aws-sdk": {
-					DynamoDB: function() {
+					DynamoDB: function () {
 						return {
-							getItem: function(params) {
-
+							scan: function () {
 								return {
-									promise: () => Promise.resolve({ Item: {
-										id: {"S": "string"},
-										number: {"N": "1234"}
-									}})
+									promise: () =>
+										Promise.resolve({
+											Items: [
+												{
+													id: { S: "string" },
+													number: { N: "1234" }
+												}
+											]
+										})
 								};
-
 							}
 						};
 					}
@@ -142,50 +104,93 @@ describe("Node simple dynamo", function() {
 			});
 		});
 
-		it("Should return the correct list", function() {
-
-			database = new ddbriver({
-			    region: "eu-west-1",
-			    dynamodb: '2012-08-10'
+		it("Should return the correct list", function () {
+			const database = new ddbriver({
+				region: "eu-west-1",
+				dynamodb: "2012-08-10"
 			});
 
-			return database.get("is-users", "test").then(function(data) {
+			return database
+				.list("is-users", [
+					{
+						key: "email",
+						operator: "EQ",
+						value: "example@super.cool"
+					}
+				])
+				.then(function (data) {
+					data.should.have.length(1);
+					data.should.have.deep.property("[0].id", "string");
+					data.should.have.deep.property("[0].number", 1234);
+				});
+		});
+	});
 
+	describe("Databse get", function () {
+		let ddbriver;
+
+		before(function () {
+			ddbriver = proxyquire(__basePath + "/index.js", {
+				"aws-sdk": {
+					DynamoDB: function () {
+						return {
+							getItem: function () {
+								return {
+									promise: () =>
+										Promise.resolve({
+											Item: {
+												id: { S: "string" },
+												number: { N: "1234" }
+											}
+										})
+								};
+							}
+						};
+					}
+				}
+			});
+		});
+
+		it("Should return the correct list", function () {
+			const database = new ddbriver({
+				region: "eu-west-1",
+				dynamodb: "2012-08-10"
+			});
+
+			return database.get("is-users", "test").then(function (data) {
 				data.should.have.property("id", "string");
 				data.should.have.property("number", 1234);
-
 			});
-
 		});
-
 	});
 
-	describe("Databse get Items with ids as input", function() {
+	describe("Databse get Items with ids as input", function () {
+		const documents = [
+			{
+				id: "123",
+				prop1: "prop1",
+				prop2: "prop2"
+			}
+		];
+		let ddbriver;
 
-		let documents = [{
-			id: "123",
-			prop1: "prop1",
-			prop2: "prop2"
-		}];
-
-		before(function() {
+		before(function () {
 			ddbriver = proxyquire(__basePath + "/index.js", {
 				"aws-sdk": {
-					DynamoDB: function() {
+					DynamoDB: function () {
 						return {
-							batchGetItem: function(params) {
-
+							batchGetItem: function () {
 								return {
-									promise: () => Promise.resolve({
-										Responses: {
-											"TABLE_NAME": utils.itemize(documents).L
-										},
-										UnprocessedKeys: {
-											"TABLE_NAME": null
-										}
-									})
+									promise: () =>
+										Promise.resolve({
+											Responses: {
+												TABLE_NAME: utils.itemize(documents).L
+											},
+											UnprocessedKeys: {
+												TABLE_NAME: null
+											}
+										})
 								};
-
 							}
 						};
 					}
@@ -193,57 +198,58 @@ describe("Node simple dynamo", function() {
 			});
 		});
 
-		it("Should return the correct list", function(	) {
-
-			database = new ddbriver({
-			    region: "eu-west-1",
-			    dynamodb: '2012-08-10'
+		it("Should return the correct list", function () {
+			const database = new ddbriver({
+				region: "eu-west-1",
+				dynamodb: "2012-08-10"
 			});
 
-			let ids = [{
-				"partition": "123",
-				"sort": "ABC"
-			}, {
-				"partition": "234",
-				"sort": "DEF"
-			}];
+			const ids = [
+				{
+					partition: "123",
+					sort: "ABC"
+				},
+				{
+					partition: "234",
+					sort: "DEF"
+				}
+			];
 
-			return database.getItems("TABLE_NAME", ids, {keys: ["partition", "sort"]}).then(function(data) {
-
-				data.should.be.deep.equal(documents);
-
-			});
-
+			return database
+				.getItems("TABLE_NAME", ids, { keys: ["partition", "sort"] })
+				.then(function (data) {
+					data.should.be.deep.equal(documents);
+				});
 		});
-
 	});
 
-	describe("Databse get Items with ids as input", function() {
+	describe("Databse get Items with ids as input", function () {
+		const documents = [
+			{
+				id: "123",
+				prop1: "prop1",
+				prop2: "prop2"
+			}
+		];
+		let ddbriver;
 
-		let documents = [{
-			id: "123",
-			prop1: "prop1",
-			prop2: "prop2"
-		}];
-
-		before(function() {
+		before(function () {
 			ddbriver = proxyquire(__basePath + "/index.js", {
 				"aws-sdk": {
-					DynamoDB: function() {
+					DynamoDB: function () {
 						return {
-							batchGetItem: function(params) {
-
+							batchGetItem: function () {
 								return {
-									promise: () => Promise.resolve({
-										Responses: {
-											"TABLE_NAME": utils.itemize(documents).L
-										},
-										UnprocessedKeys: {
-											"TABLE_NAME": null
-										}
-									})
+									promise: () =>
+										Promise.resolve({
+											Responses: {
+												TABLE_NAME: utils.itemize(documents).L
+											},
+											UnprocessedKeys: {
+												TABLE_NAME: null
+											}
+										})
 								};
-
 							}
 						};
 					}
@@ -251,148 +257,113 @@ describe("Node simple dynamo", function() {
 			});
 		});
 
-		it("Should return the correct list", function() {
-
-			database = new ddbriver({
-			    region: "eu-west-1",
-			    dynamodb: '2012-08-10'
+		it("Should return the correct list", function () {
+			const database = new ddbriver({
+				region: "eu-west-1",
+				dynamodb: "2012-08-10"
 			});
 
-			let ids = ["123", "456"];
+			const ids = ["123", "456"];
 
-			return database.getItems("TABLE_NAME", ids, {}).then(function(data) {
-
+			return database.getItems("TABLE_NAME", ids, {}).then(function (data) {
 				data.should.be.deep.equal(documents);
-
 			});
 		});
-
 	});
 
-	describe("Utils itemize", function() {
-
-		var utils = require("../utils");
-
-		it("Should execture a raound trip itemize - deitemize", function() {
-
-			var original = {
+	describe("Utils itemize", function () {
+		it("Should execute a round trip itemize - deitemize", function () {
+			const original = {
 				"null-value": null,
 				string: "string",
 				number: 1234,
-				arrayOfNumbers: [1,2,3,4,5],
-				arrayofStrings: ["a", "b", "c", "d", "e"],
+				arrayOfNumbers: [1, 2, 3, 4, 5],
+				arrayOfStrings: ["a", "b", "c", "d", "e"],
 				object: {
 					property1: "value1",
 					property2: "value2"
 				},
-				arrayObject: [{
-					string: "string",
-					number: 1234,
-					arrayOfNumbers: [1,2,3,4,5],
-					arrayofStrings: ["a", "b", "c", "d", "e"],
-					object: {
-						property1: "value1",
-						property2: "value2"
+				arrayObject: [
+					{
+						string: "string",
+						number: 1234,
+						arrayOfNumbers: [1, 2, 3, 4, 5],
+						arrayOfStrings: ["a", "b", "c", "d", "e"],
+						object: {
+							property1: "value1",
+							property2: "value2"
+						}
 					}
-				}]
+				]
 			};
 
-			var result = utils.itemize(original);
+			const result = utils.itemize(original);
 			expect(original).to.deep.equal(utils.deitemize(result));
-
 		});
 
-		it("Should execute a round trip with deitemize - itemize", function() {
-
-			var original = {
-			  "M": {
-			  	"null-value": {
-			  		"NULL": true
-			  	},
-			    "string": {
-			      "S": "string"
-			    },
-			    "number": {
-			      "N": "1234"
-			    },
-			    "arrayOfNumbers": {
-			      "NS": [
-			        "1",
-			        "2",
-			        "3",
-			        "4",
-			        "5"
-			      ]
-			    },
-			    "arrayofStrings": {
-			      "SS": [
-			        "a",
-			        "b",
-			        "c",
-			        "d",
-			        "e"
-			      ]
-			    },
-			    "object": {
-			      "M": {
-			        "property1": {
-			          "S": "value1"
-			        },
-			        "property2": {
-			          "S": "value2"
-			        }
-			      }
-			    },
-			    "arrayObject": {
-			      "L": [
-			        {
-			          "M": {
-			            "string": {
-			              "S": "string"
-			            },
-			            "number": {
-			              "N": "1234"
-			            },
-			            "arrayOfNumbers": {
-			              "NS": [
-			                "1",
-			                "2",
-			                "3",
-			                "4",
-			                "5"
-			              ]
-			            },
-			            "arrayofStrings": {
-			              "SS": [
-			                "a",
-			                "b",
-			                "c",
-			                "d",
-			                "e"
-			              ]
-			            },
-			            "object": {
-			              "M": {
-			                "property1": {
-			                  "S": "value1"
-			                },
-			                "property2": {
-			                  "S": "value2"
-			                }
-			              }
-			            }
-			          }
-			        }
-			      ]
-			    }
-			  }
+		it("Should execute a round trip with deitemize - itemize", function () {
+			const original = {
+				M: {
+					"null-value": {
+						NULL: true
+					},
+					string: {
+						S: "string"
+					},
+					number: {
+						N: "1234"
+					},
+					arrayOfNumbers: {
+						NS: ["1", "2", "3", "4", "5"]
+					},
+					arrayofStrings: {
+						SS: ["a", "b", "c", "d", "e"]
+					},
+					object: {
+						M: {
+							property1: {
+								S: "value1"
+							},
+							property2: {
+								S: "value2"
+							}
+						}
+					},
+					arrayObject: {
+						L: [
+							{
+								M: {
+									string: {
+										S: "string"
+									},
+									number: {
+										N: "1234"
+									},
+									arrayOfNumbers: {
+										NS: ["1", "2", "3", "4", "5"]
+									},
+									arrayofStrings: {
+										SS: ["a", "b", "c", "d", "e"]
+									},
+									object: {
+										M: {
+											property1: {
+												S: "value1"
+											},
+											property2: {
+												S: "value2"
+											}
+										}
+									}
+								}
+							}
+						]
+					}
+				}
 			};
 
-			var result = utils.deitemize(original);
+			const result = utils.deitemize(original);
 			expect(original).to.deep.equal(utils.itemize(result));
-
 		});
-
 	});
-
 });
